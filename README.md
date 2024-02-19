5c4aa6adf7eca4659c0765468bf600e76aa18df0
Mary Hudner <Mhudner@yahoo.com>
2024-02-19 07:47:27 -0700

    Completed First Project - Make Change

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## Make Change Project
 
 #### Description
+	For our project we were asked to make a cash register and take in values from the user to calculate amount tendered, purchase price, amount owed to customer or amount the customer still owes. We then printed the correct denominations for either process in the console.
 
 #### Technologies used
 	- Java
@@ -12,3 +13,8 @@
 
 #### Lessons Learned
  - Be mindful of indentation because it helps readability
+ - Math.Abs changes something from a negative to a positive
+ - How to convert an integer into a whole number 
+ - How to correctly use if and else if loops
+ - How to use a ternary to display the correct grammar for denominations owed
+ - I have to put a return after my if statements if I dont want them to run through to the next one
--- src/com/skilldistillery/makechange/MakeChangeHomework.java
+++ src/com/skilldistillery/makechange/MakeChangeHomework.java
@@ -1,7 +1,83 @@
 package com.skilldistillery.makechange;
 
+import java.util.Scanner;
+import java.lang.Math;
+
 public class MakeChangeHomework {
 	public static void main(String[] args) {
-		
+
+		Scanner kb = new Scanner(System.in);
+
+		System.out.println("How much is the purchase price?");
+        double purchasePrice = kb.nextDouble();
+        System.out.println("How much money was tendered from the customer?");
+        double tenderedAmount = kb.nextDouble();
+        kb.close();
+
+        int purchaseCents = (int) (purchasePrice * 100);
+        int tenderedCents = (int) (tenderedAmount * 100);
+
+        int changeCents = tenderedCents - purchaseCents;
+
+        if (changeCents < 0) {
+            System.out.println("Incorrect amount: $" + Math.abs(changeCents) / 100.0 + " still owed!");
+            return;
+        } 
+        else if (changeCents == 0) {
+            System.out.println("You've given the exact amount, thank you and have a good day!");
+            return;
+        }
+
+        System.out.println("Your change is: $" + changeCents / 100 + "." + (changeCents % 100) / 10 + changeCents % 10);
+
+	        int twentyBills = changeCents / 2000;
+	        changeCents %= 2000;
+
+	        int tenBills = changeCents / 1000;
+	        changeCents %= 1000;
+
+	        int fiveBills = changeCents / 500;
+	        changeCents %= 500;
+
+	        int oneBills = changeCents / 100;
+	        changeCents %= 100;
+
+	        int quarters = changeCents / 25;
+	        changeCents %= 25;
+
+	        int dimes = changeCents / 10;
+	        changeCents %= 10;
+
+	        int nickels = changeCents / 5;
+	        changeCents %= 5;
+
+	        int pennies = changeCents;
+
+	        if (twentyBills > 0) {
+	            System.out.println(twentyBills + (twentyBills == 1 ? " $20 Bill" : " $20 Bills"));
+	        }
+	        if (tenBills > 0) {
+	            System.out.println(tenBills + (tenBills == 1 ? " $10 Bill" : " $10 Bills"));
+	        }
+	        if (fiveBills > 0) {
+	            System.out.println(fiveBills + (fiveBills == 1 ? " $5 Bill" : " $5 Bills"));
+	        }
+	        if (oneBills > 0) {
+	            System.out.println(oneBills + (oneBills == 1 ? " $1 Bill" : " $1 Bills"));
+	        }
+	        if (quarters > 0) {
+	            System.out.println(quarters + (quarters == 1 ? " quarter" : " quarters"));
+	        }
+	        if (dimes > 0) {
+	            System.out.println(dimes + (dimes == 1 ? " dime" : " dimes"));
+	        }
+	        if (nickels > 0) {
+	            System.out.println(nickels + (nickels == 1 ? " nickel" : " nickels"));
+	        }
+	        if (pennies > 0) {
+	            System.out.println(pennies + (pennies == 1 ? " penny" : " pennies"));
+	        }
+	    }
+	
 	}
-}
+
